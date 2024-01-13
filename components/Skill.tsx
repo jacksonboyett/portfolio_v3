9c273a2b0341139af973b84c6137b2d09af07198
jacksonboyett <jackson.boyett@gmail.com>
2024-01-13 13:51:12 +0100

    styling updates to skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -13,7 +13,7 @@ function Skill({src, alt, bg, name}: SkillProps) {
     <div className={cn('h-24 w-24 flex items-center justify-center relative rounded', bg)}>
       <Image src={src} alt={alt} width={60} height={60} />
 			<div className='bg-white h-full w-full absolute opacity-0 hover:opacity-70 transition flex justify-end rounded'>
-				<p className='mt-auto mr-1 text-black font-black text-xl'>{name}</p>
+				<p className='mt-auto mr-1 text-black font-black text-lg'>{name}</p>
 			</div> 
     </div>
   );
